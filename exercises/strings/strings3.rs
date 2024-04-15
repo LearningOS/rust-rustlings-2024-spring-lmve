b4219fb6a119effc78c22065c2fd02041f43aa65
lmve <1831232566@qq.com>
2024-04-15 23:08:26 +0800

    strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,26 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    // 移除字符串两端的空白字符
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // 在字符串末尾添加 " world!"
+    let mut news = String::from(input);
+    news.push_str(" world!");
+    news
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // 将字符串中的 "cars" 替换为 "balloons"
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]
